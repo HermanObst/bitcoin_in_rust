d7a4390fa01188233332f52db548c8e2dcbf6f65
Herman Obst Demaestri <hodemaestri@gmail.com>
2023-05-31 12:18:25 -0300

    refactor data model and add test for inifinity point creation

--- src/point.rs
+++ src/point.rs
@@ -8,48 +8,25 @@ pub enum Point<const A: i64, const B: i64> {
 }
 
 impl<const A: i64, const B:i64> Point<A, B> {
-    fn new_point(x: Option<BigInt>, y: Option<BigInt>) -> Result<Self, Errors> {
-        match (x, y) {
-            (Some(x_num), Some(y_num)) => {
-                let y_squared = y_num.pow(2);
-                let x_cubed = x_num.pow(3);
-                let right_side = x_cubed + A * &x_num + B;
-
-                if y_squared != right_side {
-                    return Err(Errors::PointNotInCurve);
-            }
-            return Ok(Point::<A, B>::Point(x_num, y_num));
-            }
-            (None, Some(_y_num)) => {
-                return Err(Errors::PointNotInCurve); 
-            }
-            (Some(_x_num), None) => {
-                return Err(Errors::PointNotInCurve); 
-            }
-            (None, None) => {
-                return Ok(Point::<A,B>::Infinity)
-            }
+    fn new_point(x: BigInt, y: BigInt) -> Result<Self, Errors> {
+        // Checks if point is included in the curve y2 = x3 + ax + b
+        if y.pow(2) != x.pow(3) + A * &x + B {
+            return Err(Errors::InvalidPoint);
         }
+
+        return Ok(Point::<A, B>::Point(x, y));
+        }
+
+    fn new_infinity() -> Self {
+        Point::<A,B>::Infinity
     }
 
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
-            (
-                Point::Point(x1, y1),
-                Point::Point(x2, y2),
-            ) => x1 == x2 && y1 == y2 && A == other.get_a() && B == other.get_b(),
-            (Point::Infinity, Point::Infinity) => A == other.get_a() && B == other.get_b(),
+            (Point::Point(x1, y1), Point::Point(x2, y2)) => x1 == x2 && y1 == y2,
+            (Point::Infinity, Point::Infinity) => true,
             _ => false,
         }
-    }
-
-    // Helper methods to get the values of A and B
-    fn get_a(&self) -> i64 {
-        A
-    }
-
-    fn get_b(&self) -> i64 {
-        B
     }
 }
 
@@ -61,16 +38,21 @@ mod point_tests {
     use super::*;
 
     #[test]
-    fn test00_create_valid_point() {
-        assert!(Point::<5, 7>::new_point(Some(-1.to_bigint().unwrap()),Some(-1.to_bigint().unwrap())).is_ok());
+    fn test_create_valid_point() {
+        assert!(Point::<5, 7>::new_point(-1.to_bigint().unwrap(),-1.to_bigint().unwrap()).is_ok());
     }
 
     #[test]
-    fn test01_create_valid_point_and_check_result() {
-        let result = Point::<5, 7>::new_point(Some(-1.to_bigint().unwrap()), Some(-1.to_bigint().unwrap()));
+    fn test_create_valid_point_and_check_result() {
+        let result = Point::<5, 7>::new_point(-1.to_bigint().unwrap(), -1.to_bigint().unwrap());
         assert!(result.is_ok());
     
         let point = result.unwrap();
         assert_eq!(point, Point::<5, 7>::Point(-1.to_bigint().unwrap(), -1.to_bigint().unwrap()));
     }
+
+    #[test]
+    fn test_create_valid_point_at_infinity() {
+        assert_eq!(Point::<5,7>::new_infinity(), Point::<5,7>::Infinity);
+    } 
 }
--- src/types/errors.rs
+++ src/types/errors.rs
@@ -3,6 +3,6 @@ use thiserror::Error;
 #[derive(Debug, Error, PartialEq)]
 pub enum Errors {
     #[error("Point is not included in the curve")]
-    PointNotInCurve,
+    InvalidPoint,
 
 }
