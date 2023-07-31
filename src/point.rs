06ac95b9843cb2d14f2b28a61d7c40ba836a22ec
Herman Obst Demaestri <hodemaestri@gmail.com>
2023-07-31 20:30:25 -0300

    refactor point addition

--- src/point.rs
+++ src/point.rs
@@ -17,7 +17,7 @@ impl<const A: i64, const B:i64> Point<A, B> {
             return Err(Errors::InvalidPoint);
         }
 
-        return Ok(Point::<A, B>::Point(x, y));
+        Ok(Point::<A, B>::Point(x, y))
         }
 
     fn new_infinity() -> Self {
@@ -41,32 +41,36 @@ impl<const A: i64, const B: i64> Add<Point<A, B>> for Point<A, B> {
 
     fn add(self, other: Self) -> Self {
         match (self.clone(), other.clone()) {
-            (Point::Point(_,_), Point::Infinity) => self,
-            (Point::Infinity, Point::Point(_,_)) => other,
-            (Point::Infinity, Point::Infinity) => Point::new_infinity(),
-            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 == x2 && y1 != y2 => Point::new_infinity(),
-            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 == x2 && y1 == y2 => {
-                // Case when P1 == P2
-                let slope: BigInt = (3*&x1.pow(2) + A)/2*&y1;
+            (Point::Point(x1,y1), Point::Point(x2,y2)) => {
+                if x1 == x2 {
+                    if y1 == y2 {
+                        // Case when P1 == P2
+                        let slope: BigInt = (3*&x1.pow(2) + A)/2*&y1;
 
-                // When tanget line is vertical
-                if slope == 0.to_bigint().unwrap() {
-                    return Point::new_infinity()
+                        // When tanget line is vertical
+                        if slope == 0.to_bigint().unwrap() {
+                            return Point::new_infinity()
+                        }
+                        let x3 = slope.pow(2) - 2*&x1;
+                        let y3 = slope*(&x1 - &x3) - y1;
+                        // This unwrap cannot fail as this functions already recives two valid points.
+                        Point::new_point(x3, y3).unwrap()
+                    } else {
+                        // Vertical line (same x but different y coordinates)
+                        Point::new_infinity()
+                    }
+                } else {
+                    // Case were x coordinates are differents
+                    let slope = (&y1 - &y2)/(&x2 - &x1);
+                    let x3 = slope.pow(2) - &x1 - &x2;
+                    let y3 = slope*(&x1 - &x3) - &y1;
+                    // This unwrap cannot fail as this functions already recives two valid points.
+                    Point::new_point(x3, y3).unwrap()
                 }
-                let x3 = slope.pow(2) - 2*&x1;
-                let y3 = slope*(&x1 - &x3) - y1;
-                // This unwrap cannot fail as this functions already recives two valid points.
-                Point::new_point(x3, y3).unwrap()
             },
-            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 != x2 => {
-                // Case were x coordinates are differents
-                let slope = (&y1 - &y2)/(&x2 - &x1);
-                let x3 = slope.pow(2) - &x1 - &x2;
-                let y3 = slope*(&x1 - &x3) - &y1;
-                // This unwrap cannot fail as this functions already recives two valid points.
-                Point::new_point(x3, y3).unwrap()
-            },
-
+            // Handle identity (Infinity point). In case both are Infinity, returns Infinity (self).
+            (_, Point::Infinity) => self,
+            (Point::Infinity, _) => other
         }
     }
 }
