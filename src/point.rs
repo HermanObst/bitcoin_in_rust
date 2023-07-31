07586d8ce12cd37e537cb60ba910d0707c72b767
Herman Obst Demaestri <hodemaestri@gmail.com>
2023-07-31 19:46:16 -0300

    wip point addition

--- src/point.rs
+++ src/point.rs
@@ -1,12 +1,15 @@
 use bitcoin::types::errors::Errors;
-use num_bigint::BigInt;
+use num_bigint::{BigInt, ToBigInt};
+use core::ops::Add;
 
-#[derive(Clone, Debug, PartialEq)]
-pub enum Point<const A: i64, const B: i64> {
+#[allow(dead_code)]
+#[derive(Clone, Debug)]
+enum Point<const A: i64, const B: i64> {
     Point(BigInt, BigInt),
     Infinity
 }
 
+#[allow(dead_code)]
 impl<const A: i64, const B:i64> Point<A, B> {
     fn new_point(x: BigInt, y: BigInt) -> Result<Self, Errors> {
         // Checks if point is included in the curve y2 = x3 + ax + b
@@ -20,7 +23,10 @@ impl<const A: i64, const B:i64> Point<A, B> {
     fn new_infinity() -> Self {
         Point::<A,B>::Infinity
     }
+}
 
+#[allow(dead_code)]
+impl<const A: i64, const B: i64> PartialEq for Point<A, B> {
     fn eq(&self, other: &Self) -> bool {
         match (self, other) {
             (Point::Point(x1, y1), Point::Point(x2, y2)) => x1 == x2 && y1 == y2,
@@ -30,6 +36,40 @@ impl<const A: i64, const B:i64> Point<A, B> {
     }
 }
 
+impl<const A: i64, const B: i64> Add<Point<A, B>> for Point<A, B> {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        match (self.clone(), other.clone()) {
+            (Point::Point(_,_), Point::Infinity) => self,
+            (Point::Infinity, Point::Point(_,_)) => other,
+            (Point::Infinity, Point::Infinity) => Point::new_infinity(),
+            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 == x2 && y1 != y2 => Point::new_infinity(),
+            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 == x2 && y1 == y2 => {
+                // Case when P1 == P2
+                let slope: BigInt = (3*&x1.pow(2) + A)/2*&y1;
+
+                // When tanget line is vertical
+                if slope == 0.to_bigint().unwrap() {
+                    return Point::new_infinity()
+                }
+                let x3 = slope.pow(2) - 2*&x1;
+                let y3 = slope*(&x1 - &x3) - y1;
+                // This unwrap cannot fail as this functions already recives two valid points.
+                Point::new_point(x3, y3).unwrap()
+            },
+            (Point::Point(x1,y1), Point::Point(x2,y2)) if x1 != x2 => {
+                // Case were x coordinates are differents
+                let slope = (&y1 - &y2)/(&x2 - &x1);
+                let x3 = slope.pow(2) - &x1 - &x2;
+                let y3 = slope*(&x1 - &x3) - &y1;
+                // This unwrap cannot fail as this functions already recives two valid points.
+                Point::new_point(x3, y3).unwrap()
+            },
+
+        }
+    }
+}
 
 #[cfg(test)]
 mod point_tests {
