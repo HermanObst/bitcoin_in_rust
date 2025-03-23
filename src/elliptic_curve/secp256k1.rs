cb5a0d0810433993999682c27d82bb71ca1528f1
Herman Obst Demaestri <hodemaestri@gmail.com>
2025-03-23 00:15:09 -0300

    define secp256k1 curve. Add tests for Bitcoin curve

--- src/elliptic_curve/secp256k1.rs
+++ src/elliptic_curve/secp256k1.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use crate::elliptic_curve::{
     finite_field::FieldElement, traits::Point, weierstrass_field_point::WeierstrassCurve,
 };
@@ -21,12 +22,13 @@ static SECP256K1_CURVE: Lazy<WeierstrassCurve> = Lazy::new(|| WeierstrassCurve {
     b: SECP256K1_B.clone(),
 });
 
+
 const SECP256K1_X_GENERATOR_HEX: &str =
-    "0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
+    "79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798";
 const SECP256K1_Y_GENERATOR_HEX: &str =
-    "0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8";
+    "483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8";
 const SECP256K1_ORDER_HEX: &str =
-    "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141";
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141";
 
 static SECP256K1_GX: Lazy<FieldElement> = Lazy::new(|| {
     FieldElement::new(
@@ -43,5 +45,29 @@ static SECP256K1_GY: Lazy<FieldElement> = Lazy::new(|| {
 static SECP256K1_ORDER: Lazy<BigInt> =
     Lazy::new(|| BigInt::from_str_radix(SECP256K1_ORDER_HEX, 16).unwrap());
 
-static SECP256K1_GENERATOR: Lazy<Point<WeierstrassCurve>> =
-    Lazy::new(|| Point::new_point(&SECP256K1_CURVE, &SECP256K1_GX, &SECP256K1_GY).unwrap());
+static SECP256K1_GENERATOR: Lazy<Point<WeierstrassCurve>> = Lazy::new(|| {
+    Point::<WeierstrassCurve>::new_point(&SECP256K1_CURVE, &SECP256K1_GX, &SECP256K1_GY).unwrap()
+});
+
+#[cfg(test)]
+mod elliptic_curve_tests {
+    use num_bigint::ToBigInt;
+
+    use super::*;
+
+    #[test]
+    fn test_multiply_secp256k1_curve_generator_by_order_returns_infinity() {
+        assert_eq!(
+            Point::<WeierstrassCurve>::new_infinity(&SECP256K1_CURVE),
+            SECP256K1_GENERATOR.clone() * SECP256K1_ORDER.clone()
+        )
+    }
+
+    #[test]
+    fn test_multiply_secp256k1_curve_generator_by_order_plus1_returns_circles_back() {
+        assert_eq!(
+            SECP256K1_GENERATOR.clone(),
+            SECP256K1_GENERATOR.clone() * (SECP256K1_ORDER.clone() + 1.to_bigint().unwrap())
+        )
+    }
+}
